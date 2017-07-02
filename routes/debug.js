0bda88b0e0cb1a8a039893fe658332e8dc1ec90a
Jon Peck <jpeck@fluxsauce.com>
2017-07-01 23:06:51 -0700

    Debug

--- routes/debug.js
+++ routes/debug.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 var debug = require('debug')('nadia:debug')
 const db = require('sqlite');
+const _ = require('lodash');
 
 /* GET debug listing. */
 router.get('/', function(req, res, next) {
   db.all('SELECT * FROM Reservation')
     .then((reservations) => {
       debug(reservations);
-      res.send(reservations)
+      debug(_.keys(reservations[0]));
+      res.render('debug', {
+        title: 'Requests - Nadia\'s Garden',
+        reservations,
+        header: _.keys(reservations[0])
+      });
     })
 });
 
