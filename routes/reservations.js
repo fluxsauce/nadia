cda41b19dc4ae10109325a304d14bdacb0749274
Jon Peck <jpeck@fluxsauce.com>
2017-07-01 22:15:46 -0700

    Persist submissions.

--- routes/reservations.js
+++ routes/reservations.js
@@ -2,27 +2,40 @@ var express = require('express');
 var router = express.Router();
 var debug = require('debug')('nadia:reservations')
 const db = require('sqlite');
+let _ = require('lodash');
+var moment = require('moment');
 
-/* GET reservations listing. */
 router.get('/', function(req, res, next) {
-  res.render('reservations', { title: 'Reservations – Nadia\'s Garden' });
+  res.render('reservations');
 });
 
 router.post('/', function(req, res, next) {
-  const values = [
-    'July 3 2017 05BUG0pm',
-    3,
-    'NAME',
-    'username@example.com',
-    '555 555 5555',
-    'yay'
-  ]
-  db.run('INSERT INTO Reservation (datetime, party, name, email, phone, message) VALUES (?, ?, ?, ?, ?, ?)', values)
-    .then(debug(values))
-    .then(res.send('respond with a resource'))
+  const submission = _.pick(req.body, [ 'party', 'name', 'email', 'phone', 'message']);
+  submission.datetime = moment(`${req.body.date} ${req.body.time}`, 'YYYY/MM/DD H:mm A').toString();;
+  
+  debug(submission);
+  
+  const columns = _.keys(submission);
+  const values = _.values(submission);
+  const placeholders = Array(values.length).fill('?')
+
+  const sql = `INSERT INTO Reservation (${columns.join(', ')}) VALUES (${placeholders.join(', ')})`
+  
+  db.run(sql, values)
+    .then(() => {
+      res.render('reservations', {
+        success: true,
+      });
+    })
     .catch(err => {
       debug(err);
-      res.status(400).send(err.message)
+      res.status(400).render('reservations', {
+        errors: [
+          { msg: err }
+        ],
+        success: false,
+        submission: req.body
+      });
     }
     )
 });
