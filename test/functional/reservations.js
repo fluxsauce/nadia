0834c1c33a762f03114b03c8c110706cb22a3ee2
Jon Peck <jpeck@fluxsauce.com>
2017-07-11 23:47:41 -0700

    This works.

--- test/functional/reservations.js
+++ test/functional/reservations.js
@@ -1,8 +1,9 @@
+process.env['DEBUG'] = 'nadia:*';
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const proxyquire = require('proxyquire');
 const sinon = require('sinon');
-const db = require('sqlite');
 const logger = require('morgan');
 
 chai.use(chaiHttp);
@@ -10,25 +11,42 @@ chai.use(chaiHttp);
 const should = chai.should();
 
 describe('/reservations', function() {
-  const dbStub = sinon.stub(db, 'run').resolves({
-    stmt: {
-      lastID: 1349
-    }
-  });
-  const loggerStub = sinon.stub(logger, 'morgan').returns(function(req, res, next) {
-    next();
-  });
+  let dbStub;
+  let loggerStub;
+  let debugStub;
+
   let app;
 
   before(function() {
+    dbStub = {
+      run: function() {
+        return Promise.resolve({
+          stmt: {
+            lastID: 1349
+          }
+        });
+      }
+    }
+    dbStub['@global'] = true;
+
+    loggerStub = sinon
+      .stub(logger, 'morgan')
+      .returns(function(req, res, next) {
+        next();
+      });
+    debugStub = function() {
+      return sinon.stub();
+    }
+    debugStub['@global'] = true;
+
     app = proxyquire('../../app', {
       sqlite: dbStub,
       morgan: loggerStub,
+      debug: debugStub
     });
   });
 
   after(function() {
-    dbStub.restore();
     loggerStub.restore();
   });
 
